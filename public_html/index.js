0b2d819b1083376301b09642c95e98513ac6124f
EnriqueTests <enriquenada@outlook.es>
2021-05-31 11:53:15 -0500

    Uso de pop en array

--- public_html/index.js
+++ public_html/index.js
@@ -6,9 +6,13 @@
 
 // http://localhost:8383/Curso_Javascript/index.html
 
-var array = ["Stimpson", "J", "cat"];
-// Equivale a ["Stimpson", "J", "cat", ["happy", "joy"]]
-array.push(["happy", "joy"]);
+var array = [1, 2, 3];
+
+// elementoRemovido es igual a 3 y array ahora es [1, 2]]
+var elementoRemovido = array.pop();
 
 var miArray = [["John", 23], ["cat", 2]];
-miArray.push(["dog", 3]);
+var elementoRemovidoDeMiArray = miArray.pop();
+
+// Imprime [["John", 23]]
+console.log(miArray);
