034c4eb5e5d9ee1b049ed70bcdfdd8f212124ffd
EnriqueTests <enriquenada@outlook.es>
2021-05-31 12:26:04 -0500

    Uso de funciones

--- public_html/index.js
+++ public_html/index.js
@@ -6,4 +6,10 @@
 
 // http://localhost:8383/Curso_Javascript/index.html
 
-var lista = [["cereal", 3], ["leche", 2], ["bananas", 3], ["jugo", 2], ["huevos", 5]];
+function funcionReusable() {
+    console.log("Hola mundo");
+}
+
+funcionReusable();
+funcionReusable();
+funcionReusable();
