f883abead3851720ad2855fb44becffd27a24992
EnriqueTests <enriquenada@outlook.es>
2021-06-03 13:15:24 -0500

    Creacion de un perfil

--- public_html/index.js
+++ public_html/index.js
@@ -6,12 +6,43 @@
 
 // http://localhost:8383/Curso_Javascript/index.html
 
-var arreglo = [];
-var i = 0;
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknow",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
 
-do {
-    arreglo.push(i);
-    i++;
-} while(i < 5);
+function lookUpProfile(name, prop) {
+    for(var i = 0; i < contacts.length; i++) {
+        if(contacts[i].firstName === name) {
+            return contacts[i][prop] || "No such property";
+        }
+    }
 
-console.log(i, arreglo);
+    return "No such contact";
+}
+
+var data = lookUpProfile("Akira", "likes");
+
+console.log(data);
