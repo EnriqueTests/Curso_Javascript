735bc1b14f860b2b5304506ed0a1f151b0e3f2de
EnriqueTests <enriquenada@outlook.es>
2021-06-02 23:12:13 -0500

    Uso de un bucle while

--- public_html/index.js
+++ public_html/index.js
@@ -6,47 +6,13 @@
 
 // http://localhost:8383/Curso_Javascript/index.html
 
-var collection = {
-    "2548": {
-        "album": "Slipper When Wet",
-        "artist": "Bon Jovi",
-        "tracks": [
-            "Let It Rock",
-            "You Give Love a Bad Name"
-        ]
-    },
-    "2468": {
-        "album": "1999",
-        "artist": "Prince",
-        "tracks": [
-            "1999",
-            "Little Red Corvette"
-        ]
-    },
-    "1245": {
-        "artist": "Robert Palmer",
-        "tracks": [ ]
-    },
-    "5439": {
-        "album": "ABBA Gold"
-    }
-};
+var array = [];
 
-// Copia de la coleccion para pruebas
-var collectionCopy = JSON.parse(JSON.stringify(collection));
+var i = 0;
 
-function updateRecords(id, prop, value) {
-    if(value === "") {
-        delete collection[id][prop];
-    } else if(prop === "tracks") {
-        collection[id][prop] = collection[id][prop] || [];
-        collection[id][prop].push(value);
-    } else {
-        collection[id][prop] = value;
-    }
-
-    return collection;
+while(i < 5) {
+    array.push(i);
+    i++;
 }
 
-updateRecords(2468, "tracks", "test");
-console.log(updateRecords(5439, "artist", "ABBA"));
+console.log(array);
