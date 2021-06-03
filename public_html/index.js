806177e7c9c559717fe03cba4050bc33a169b67e
EnriqueTests <enriquenada@outlook.es>
2021-06-03 16:16:18 -0500

    Funciones flecha con argumentos

--- public_html/index.js
+++ public_html/index.js
@@ -6,4 +6,6 @@
 
 // http://localhost:8383/Curso_Javascript/index.html
 
-const magic = () => new Date();
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
