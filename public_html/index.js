7df4ad4b05f2db9003d60f8433d8b0203bef8895
EnriqueTests <enriquenada@outlook.es>
2021-06-03 16:13:30 -0500

    Funcion flecha

--- public_html/index.js
+++ public_html/index.js
@@ -6,22 +6,4 @@
 
 // http://localhost:8383/Curso_Javascript/index.html
 
-function freezeObj() {
-    "use strict";
-
-    const MATH_CONSTANTS = {
-        PI: 3.14
-    };
-
-    Object.freeze(MATH_CONSTANTS);
-
-    try {
-        MATH_CONSTANTS.PI = 99;
-    } catch (ex) {
-        console.log(ex);
-    }
-
-    return MATH_CONSTANTS.PI;
-}
-
-const PI = freezeObj();
+const magic = () => new Date();
