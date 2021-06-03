44f90f403803599ea67b9b8ed4941156895ba079
EnriqueTests <enriquenada@outlook.es>
2021-06-03 13:20:39 -0500

    Generar numero aleatorio entre 0 y 1

--- public_html/index.js
+++ public_html/index.js
@@ -6,43 +6,8 @@
 
 // http://localhost:8383/Curso_Javascript/index.html
 
-var contacts = [
-    {
-        "firstName": "Akira",
-        "lastName": "Laine",
-        "number": "0543236543",
-        "likes": ["Pizza", "Coding", "Brownie points"]
-    },
-    {
-        "firstName": "Harry",
-        "lastName": "Potter",
-        "number": "0994372684",
-        "likes": ["Hogwarts", "Magic", "Hagrid"]
-    },
-    {
-        "firstName": "Sherlock",
-        "lastName": "Holmes",
-        "number": "0487345643",
-        "likes": ["Intriguing Cases", "Violin"]
-    },
-    {
-        "firstName": "Kristian",
-        "lastName": "Vos",
-        "number": "unknow",
-        "likes": ["JavaScript", "Gaming", "Foxes"]
-    }
-];
-
-function lookUpProfile(name, prop) {
-    for(var i = 0; i < contacts.length; i++) {
-        if(contacts[i].firstName === name) {
-            return contacts[i][prop] || "No such property";
-        }
-    }
-
-    return "No such contact";
+function randomFraction() {
+    return Math.random();
 }
 
-var data = lookUpProfile("Akira", "likes");
-
-console.log(data);
+console.log(randomFraction());
