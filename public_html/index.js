6c3c47151cd05207e616a0234b12792c6e54e1e9
EnriqueTests <enriquenada@outlook.es>
2021-06-03 16:24:41 -0500

    Funciones flecha de orden superior

--- public_html/index.js
+++ public_html/index.js
@@ -6,6 +6,13 @@
 
 // http://localhost:8383/Curso_Javascript/index.html
 
-const myConcat = (arr1, arr2) => arr1.concat(arr2);
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 
-console.log(myConcat([1, 2], [3, 4, 5]));
+const squareList = (arr) => {
+    const squqredIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x);
+    
+    return squqredIntegers;
+};
+
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
