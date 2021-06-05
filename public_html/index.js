0ad8691d0406108c63f081b26aba86a8f049a41f
EnriqueTests <enriquenada@outlook.es>
2021-06-05 12:34:57 -0500

    Asignacion de desestructuracion de objetos anidado

--- public_html/index.js
+++ public_html/index.js
@@ -6,25 +6,17 @@
 
 // http://localhost:8383/Curso_Javascript/index.html
 
-var voxel = {x: 3.6, y: 7.4, z: 6.54};
-
-var x = voxel.x; // x = 3.6
-var y = voxel.y; // y = 7.4
-var z = voxel.z; // z = 6.54
-
-const { x : a, y : b, z : c} = voxel; // a = 3.6, b = 7.4, c = 6.54
-
-const AVG_TEMPERATURES = {
-    today: 77.5,
-    tomorrow: 79
+const LOCAL_FORECAST = {
+    today: { min : 72, max : 83 },
+    tomorrow: {min : 73.3, max : 84.6}
 };
 
-function getTempOfTmrw(avgTemperatures) {
+function getMaxOfTmrw(forecast) {
     "use strict";
 
-    const { tomorrow : tempOfTomorrow } = avgTemperatures;
+    const { tomorrow : { max : maxOfTomorrow }} = forecast;
 
-    return tempOfTomorrow;
+    return maxOfTomorrow;
 }
 
-console.log(getTempOfTmrw(AVG_TEMPERATURES));
+console.log(getMaxOfTmrw(LOCAL_FORECAST));
