0571c814ab23f3e049836c30f2368a7e6719c494
EnriqueTests <enriquenada@outlook.es>
2021-06-05 15:21:49 -0500

    export e import

--- public_html/index.js
+++ public_html/index.js
@@ -6,40 +6,8 @@
 
 // http://localhost:8383/Curso_Javascript/index.html
 
-class Book {
-    constructor(author) {
-        this._author = author;
-    }
+import { capitalizeString } from "./string_function"
 
-    get writer() {
-        return this._author;
-    }
+const cap = capitalizeString("hello!");
 
-    set writer(updatedAuthor) {
-        this._author = updatedAuthor;
-    }
-}
-
-function makeClass() {
-    class Thermostat {
-        constructor(temp) {
-            this._temp = 5/9 * (temp - 32);
-        }
-
-        get temperature() {
-            return this._temp;
-        }
-
-        set temperature(updatedTemp) {
-            this._temp = updatedTemp;
-        }
-    }
-
-    return Thermostat;
-}
-
-const Thermostat = makeClass();
-const thermos = new Thermostat(76);
-let temp = thermos.temperature;
-thermos.temperature = 76;
-temp = thermos.temperature;
+console.log(cap);
